0bddaebf1d6df952f708900761a376c2d4a199a4
jiwei-hemeng <jiwei7065@sina.com>
2021-01-09 21:25:14 +0800

    集成uview组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,7 @@
 			<text class="title">{{title}}</text>
 		</view>
     <uni-pagination title="标题文字" total="20"></uni-pagination>
+    <u-button type="success" class="custom-style">成功按钮</u-button>
 	</view>
 </template>
 
@@ -28,27 +29,9 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss">
+  .custom-style {
+    color: #606266;
+    width: 400rpx;
+  }
 </style>
